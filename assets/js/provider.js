d92d93673898391a83e03b0cb8dac8581d599ab3
Alia Ward <alia.ward@nike.com>
2017-12-02 14:29:32 -0800

    added provider functionality

---
+++ assets/js/provider.js
@@ -0,0 +1,31 @@
+/*
+	Prologue by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
+
+(function($) {
+
+console.log("this provider is running");
+$('#savePlace').on('click', function(event){
+	//event.preventDefault()
+	console.log('i got a click')
+	var checkedVals = $(':checkbox:checked').map(function() {
+					 return this.id;
+			 }).get();
+			console.log(checkedVals);
+			var pushObject = {
+				name : "new location",
+				id : locationStore.length + 1,
+				services : checkedVals,
+				position : {
+					lat : 45.5,
+					long : -122.7
+				},
+				info : ""
+			}
+			locationStore.push(pushObject);
+			console.log(locationStore);
+})
+
+})(jQuery);
